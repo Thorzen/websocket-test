47e9a16dd28e34c2a838129f38570f847fbf75d7
jthor <jthorsen@outlook.com>
2018-01-28 20:55:19 -0500

    Basic guess number game

--- server/index.js
+++ server/index.js
@@ -7,7 +7,16 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
+
+  var min = 1;
+  var max = 100;
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  socket.on('message', function (num) {
+    num = parseInt(num, 10);
+    socket.emit('guessresponse', num === randomNum ? 'You got it!' : (num < randomNum ? 'Too low' : 'Too high'))
+  });
 });
 
 http.listen(3000, function() {
